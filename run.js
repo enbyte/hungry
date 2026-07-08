8c2148951b65b88c680545262fd8ffb63d451b67
Enbyte Games <83731959+enbyte@users.noreply.github.com>
2026-07-08 00:52:55 -0400

    run.js: use argv

--- run.js
+++ run.js
@@ -3,16 +3,20 @@ const { readFileSync } = require('fs');
 const { IR } = require('./infra/ir.js');
 const { compile } = require('./infra/compiler.js');
 const { execute } = require('./infra/executor.js');
+const { argv } = require('process');
 
-const FILE = "script.js";
+const FILE = argv[2];
 const src = readFileSync(FILE, {encoding: 'utf8'});
+
 const ir = new IR(src, [
     // IR passes go here
 ]);
 
-console.log(src);
+console.log(ir.toString());
+
 let [bc, pool] = compile(ir, [
     // Compiler passes go here
 ]);
+
 console.log('Executing...');
-execute(bc, pool);
+console.log(execute(bc, pool));
